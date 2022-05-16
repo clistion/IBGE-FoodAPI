76bdc341850059f5e2ea4b6656c8e7b17e719d26
Mizael Clistion <clistion@gmail.com>
2022-05-16 00:50:28 -0300

    seed category_id into food table and shows in json response

--- database/seeders/FoodSeeder.php
+++ database/seeders/FoodSeeder.php
@@ -9,6 +9,7 @@ use App\Models\Food;
 use File;
 
 use App\Models\Category;
+use Illuminate\Support\Arr;
 
 class FoodSeeder extends Seeder
 {
@@ -24,11 +25,25 @@ class FoodSeeder extends Seeder
         $json = File::get("database/data/foods.json");
         $foods = json_decode($json);
 
+        $json2 = File::get("database/data/categories_x_foodsCode.json");
+        $foods_categories = json_decode($json2);
+
+
+
         foreach ($foods as $key => $value) {
+            $id = 0;
+
+            foreach ($foods_categories as $k => $fc) {
+                if ($fc->food_code == $value->code) {
+                    $id = $fc->category_id;
+                }
+            }
+
             Food::create([
                 "code" => $value->code,
                 "description" => $value->description,
-                "preparation_code" => $value->preparation_code
+                "preparation_code" => $value->preparation_code,
+                "category_id" => $id
             ]);
         }
     }
